DerpFest-Devices/device_xiaomi_sdm660-common

sdm660-common: Add shim for libdpmframework
Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

extract-files.sh
@@ -62,6 +62,10 @@ fi
 function blob_fixup() {
     case "${1}" in
 
+    product/lib64/libdpmframework.so)
+        patchelf --add-needed libdpmframework_shim.so "${2}"
+        ;;
+
     vendor/bin/mlipayd@1.1)
         patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
         ;;

Jasmine-Wayne-4-19/android_device_xiaomi_sdm660_common_4.19

Change-Id: I5bbfcfafd3898d00b489df2cd057ca6dc043e21c

extract-files.sh
@@ -74,6 +74,10 @@ fi
 function blob_fixup() {
     case "${1}" in
 
+    product/lib64/libdpmframework.so)
+        patchelf --add-needed libcutils_shim.so "${2}"
+        ;;
+
     vendor/bin/mlipayd@1.1)
         patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
         ;;