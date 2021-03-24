CarbonROM/android_device_xiaomi_sdm660-common

Revert "sdm660: Update clang to 10.0.6"
This reverts commit 2f3bc77532b1b949b3cd76b339c0bcc8ca9b276c.

Reason for revert: <INSERT REASONING HERE>

Change-Id: Ic51a70977d15428bd9e36c6a1ce248be6b5dc4c0

BoardConfigCommon.mk
@@ -94,7 +94,7 @@ HEALTHD_USE_BATTERY_INFO := true
 
 # Clang
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := r377782d
+TARGET_KERNEL_CLANG_VERSION := r377782b
 
 # CNE and DPM
 BOARD_USES_QCNE := true

Jasmine-Wayne-4-19/android_device_xiaomi_sdm660_common_4.19

BoardConfigCommon.mk
@@ -106,7 +106,7 @@ HEALTHD_USE_BATTERY_INFO := true
 
 # Clang
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := r377782d
+TARGET_KERNEL_CLANG_VERSION := r377782c
 
 # CNE and DPM
 BOARD_USES_QCNE := true