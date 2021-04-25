3a74005157ed99ad1cf17f0e5fa21e0c056caf3e
Mahesh Sharma <smahesh@codeaurora.org>
2021-04-25 12:56:58 +0800

    sdm660-common: ANT: Support new ANT stack

--- sdm660.mk
+++ sdm660.mk
@@ -132,8 +132,9 @@ PRODUCT_COPY_FILES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
+    AntHalService-Soong \
+    com.dsi.ant.antradio_library \
+    com.dsi.ant@1.0.vendor    
 
 # ANT Permission
 PRODUCT_COPY_FILES += \
