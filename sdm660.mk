ba43eaca833c0656f5cae978a23a6e1aba07baf5
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-06-05 22:47:52 +0800

    sdm660-common: Speed preopt SysUI/Launcher/Settings
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -394,6 +394,12 @@ DEVICE_PACKAGE_OVERLAYS += \
 TARGET_USES_RRO := true
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Dexpreopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Launcher3QuickStep \
+    Settings \
+    SystemUI
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
@@ -456,9 +462,6 @@ else
     PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/power-libperfmgr/sdm660_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 endif
-
-# Preopt SystemUI
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 
 # Protobuf
 PRODUCT_PACKAGES += \
